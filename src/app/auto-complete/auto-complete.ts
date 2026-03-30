d7cd85d3085186d69b9140d73d8b85cb7e7fd14e
Alicia Bosch <alicia.bosch@isen.yncrea.fr>
2026-03-30 09:47:45 +0200

    ajout des composants

---
+++ src/app/auto-complete/auto-complete.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auto-complete',
+  imports: [],
+  templateUrl: './auto-complete.html',
+  styleUrl: './auto-complete.css',
+})
+export class AutoComplete {
+
+}
---
+++ src/app/switch-map/switch-map.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-map',
+  imports: [],
+  templateUrl: './switch-map.html',
+  styleUrl: './switch-map.css',
+})
+export class SwitchMap {
+
+}
